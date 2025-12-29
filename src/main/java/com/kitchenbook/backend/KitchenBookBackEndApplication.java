016d6edbac7e174183aa3edc847f74678db694b4
gesimon81 <61974460+gesimon81@users.noreply.github.com>
2025-12-29 06:13:04 +0100

    Backend SpringBoot start

---
+++ src/main/java/com/kitchenbook/backend/KitchenBookBackEndApplication.java
@@ -0,0 +1,14 @@
+package com.kitchenbook.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenBookBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchenBookBackEndApplication.class, args);
+		System.out.println("Application started");
+	}
+
+}
